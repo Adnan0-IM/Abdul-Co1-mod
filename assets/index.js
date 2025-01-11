fd5cbe5e76decd426046ffc20c44ff0cae96857a
Adnan Iliyasu Muhammad <adnanilyas969@gmail.com>
2025-01-11 20:43:36 +0100

    update mobile menu

---
+++ assets/index.js
@@ -0,0 +1,11 @@
+
+          
+                const hamburgerMenu = document.getElementById("hamburgerMenu");
+        const mobileMenu = document.getElementById("mobileMenu");
+
+        hamburgerMenu.addEventListener("click", () => {
+            mobileMenu.classList.toggle("hidden");
+        });
+
+       
+
